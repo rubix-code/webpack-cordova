3e68cef9f7f96090bf7f8a0280fa755d8b9e2d0f
Rinzler <rinzler.d.vicky@gmail.com>
2018-01-12 19:53:04 +0530

    Added : vue-router can now send props

--- template/src/.default/router/pages.js
+++ template/src/.default/router/pages.js
@@ -5,6 +5,7 @@ files.keys().forEach(key => {
 	if(files(key).default.route && files(key).default.route.name && files(key).default.route.path ){
 		pages.push({
 			...files(key).default.route,
+			props: true,
 			component:files(key){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 		}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 	}
